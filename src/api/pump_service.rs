d57d318fc67eb6d746a58febc6d425ec6c7eca53
umbreon222 <umbreon222@gmail.com>
2022-06-20 11:44:02 -0500

    Add notes

--- src/api/pump_service.rs
+++ src/api/pump_service.rs
@@ -31,6 +31,8 @@ pub struct PumpService {
 // TODO: This shit is hella rough so we should clean it up at some point.
 // TODO: Cases we need to think about:
 // 1. Double call to enqueue_pump() with same pump number.
+//   a. Solution to this is to add duration to the enqueue_pump() call.
+//   b. Add a get method for the pump queue (api).
 impl PumpService {
     pub fn new() -> Self {
         let pi = wiringpi::setup();
