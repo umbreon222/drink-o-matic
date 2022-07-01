88c3340510c711334adc5c706bde0904e557cead
umbreon222 <umbreon222@users.noreply.github.com>
2022-07-01 16:48:01 -0500

    Added logging + ability to mock GPIO (#1)

---
+++ src/mock/line.rs
@@ -0,0 +1,9 @@
+use crate::mock::{ LineHandle, LineRequestFlags };
+
+pub struct Line { }
+
+impl Line {
+    pub fn request(&self, _args: LineRequestFlags, _value: u8, _name: &str) -> Result<LineHandle, &str> {
+        Ok(LineHandle { })
+    }
+}
