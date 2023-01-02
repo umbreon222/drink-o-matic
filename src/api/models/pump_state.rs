e947e19d092d0f7e5c941bca8f441dc4f232ba5e
umbreon222 <umbreon222@users.noreply.github.com>
2023-01-01 23:59:10 -0600

    Fix pump state serialization so it outputs as camelCase (#13)

--- src/api/models/pump_state.rs
+++ src/api/models/pump_state.rs
@@ -2,6 +2,8 @@ use serde::Serialize;
 
 #[derive(Serialize, Clone)]
 pub struct PumpState {
+    #[serde(rename = "pumpNumber")]
     pub pump_number: u8,
+    #[serde(rename = "isRunning")]
     pub is_running: bool
 }
